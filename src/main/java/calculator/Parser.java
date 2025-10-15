167a49078420bc243c42117228e355f73cc82c42
Junha Park <poiu7825@pusan.ac.kr>
2025-10-15 19:37:27 +0900

    fix: add escaping logic for special character in Parser

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -1,6 +1,10 @@
 package calculator;
 
 
+import javax.sound.midi.SysexMessage;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 입력된 문자열을 구분자를 기준으로 파싱하는 클래스
  */
@@ -14,6 +18,11 @@ public class Parser {
         int end = input.indexOf("\\n");
 
         String delimiter = input.substring(start, end);
+        String specialChars = "([\\Q" + "^$|(){}[]*+?.\\" + "\\E])";
+        Matcher matcher = Pattern.compile(specialChars).matcher(delimiter);
+        delimiter = matcher.replaceAll("\\\\$1");
+        System.out.println(delimiter);
+
 
         String remain = input.substring(end + 2);
         return remain.split(delimiter);
