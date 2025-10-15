5b3b3592b16d81aaa15054df59b2ccf7b2e31c92
Junha Park <poiu7825@pusan.ac.kr>
2025-10-15 14:33:51 +0900

    style: add OutputHandler description comments

--- src/main/java/calculator/OutputHandler.java
+++ src/main/java/calculator/OutputHandler.java
@@ -1,7 +1,11 @@
 package calculator;
 
+
+/**
+ * 결과를 출력하는 클래스
+ */
 public class OutputHandler {
-    public void output(Integer rst){
-        System.out.println("결과 : " + rst);
+    public void output(Integer result){
+        System.out.println("결과 : " + result);
     }
 }
